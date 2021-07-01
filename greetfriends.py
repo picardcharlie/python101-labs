cf367be0b166d2f0bf21a8bda27603a14927a6d6
picardcharlie <86638102+picardcharlie@users.noreply.github.com>
2021-06-30 19:52:41 -0500

    testing how github works, adding the lesson code

---
+++ greetfriends.py
@@ -0,0 +1,10 @@
+# Collect the names of my friends
+# Decide how to greet them
+# Print the greeting with each of their names
+
+friends = ["Stevesy", "Medge", "Marcus"]
+
+greeting = "Yo what up, "
+
+for name in friends:
+    print(greeting, name)
