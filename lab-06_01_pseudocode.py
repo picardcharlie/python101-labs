6594d792af1b986808e72ac74b70cfe0272dd682
picardcharlie <86638102+picardcharlie@users.noreply.github.com>
2021-07-02 22:25:00 -0500

    Add files via upload

---
+++ lab-06_01_pseudocode.py
@@ -0,0 +1,18 @@
+# Your cat just had kittens! Now you want to put up an ad to give them
+# to your friends. You'll need to save all names of the kittens,
+# confirm that they each of them is cute, and show a message that
+# that kitten is ready for adoption.
+#
+# Break this task up into a couple of steps of pseudocode
+# and write the pseudocode below in code comments.
+# You don't need to write any functional code, just map out the steps.
+
+##########
+
+# Make a list of all the kitten names
+
+# Give each kitten a cute value from yes to extremely
+
+# Send out a message to my friends that each kitten is cute
+
+# Update the message whenever a kitten is adopted
