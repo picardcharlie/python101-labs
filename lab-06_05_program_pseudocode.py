71072d9acc1325ccffddd365eabd16b68e3bfbae
picardcharlie <86638102+picardcharlie@users.noreply.github.com>
2021-07-03 23:23:21 -0500

    Add files via upload

---
+++ lab-06_05_program_pseudocode.py
@@ -0,0 +1,24 @@
+# Think about a task that you want to accomplish using programming
+# Maybe you want to automate something you do every day on your computer?
+# Break down some of the steps you can think of and write them in
+# this file as pseudocode.
+# You can come back and update this file when you've learned more.
+# You can also add real Python code after learning the necessary concepts.
+# For now, just practice breaking larger tasks into smaller steps
+# and writing out your thoughts in pseudocode.
+
+#####################################################
+
+# A logging script to keep track of my 590 mining eth.
+# Track minimum and maximum mining rate, highest difficulty of share,
+# the amount mined each day and averages for different lengths of time.
+
+# Check mining pool every ten minutes (or whatever the update rate is) and pull hashrate and current balance.
+# Write information to a log.
+
+# Pull highest share difficulty from mining program log and write it to log.
+
+# Pull data from new log and format it on a site I can check.
+# Show highest hash rate from the last week, highest ever.  Averages over last day, week, month.
+# Find which hours of the day result in highest and lowest averages.
+# Show current balance, actual daily average, weekly, monthly.
